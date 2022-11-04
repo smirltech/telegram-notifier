a3ab021b2553525a38fb7090179a1bdf10ab1bd7
Marien Mupenda <marien.mupenda@gmail.com>
2022-11-04 14:57:45 +0200

    add sender to message

--- app/Data/Payload.php
+++ app/Data/Payload.php
@@ -52,7 +52,7 @@ class Payload
     public function content(): string
     {
         // create a message content from attributes using md
-        $content = "[$this->repository]($this->repositoryUrl)\n";
+        $content = "*{$this->sender}* on [$this->repository]($this->repositoryUrl)\n";
         $content .= "\n";
         $content .= "_{$this->message}_ \n\n";
         $content .= "Commits: " . count($this->commits) . ", ";
